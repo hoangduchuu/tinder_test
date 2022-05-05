e34f67e52d7b12495e83e41b39e1978f71f2d239
Huu <hoangduchuuvn@gmail.com>
2022-05-05 13:39:44 +0700

    [DEV] implement db viewer
    easy view db in flutter app 
    https://pub.dev/packages/sqlite_viewer

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:m_tinder/ui/home/home_controller.dart';
+import 'package:sqlite_viewer/sqlite_viewer.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeController controller = Get.find<HomeController>();
@@ -8,34 +9,46 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => Scaffold(
-        body: controller.users.value == null
-            ? Container()
-            : ListView.builder(
-                itemBuilder: (ctx, index) {
-                  var item = controller.users[index];
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text("${item.title} \t ${item.lastName}"),
-                      ElevatedButton(
-                        onPressed: () {
-                          controller.ignore(item);
-                        },
-                        child: Text('Like'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          controller.like(item);
-                        },
-                        child: Text('DisLike'),
-                      )
-                    ],
-                  );
-                },
-                itemCount: controller.users.value.length,
-              ),
-      ),
+          () =>
+          Scaffold(
+            appBar: AppBar(
+              actions: [
+                InkWell(
+
+                  child: Icon(Icons.table_rows_outlined),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => DatabaseList()));
+                  },
+                )
+              ],
+            ),
+            body: controller.users.value == null
+                ? Container()
+                : ListView.builder(
+              itemBuilder: (ctx, index) {
+                var item = controller.users[index];
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("${item.title} \t ${item.lastName}"),
+                    ElevatedButton(
+                      onPressed: () {
+                        controller.ignore(item);
+                      },
+                      child: Text('Like'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        controller.like(item);
+                      },
+                      child: Text('DisLike'),
+                    )
+                  ],
+                );
+              },
+              itemCount: controller.users.value.length,
+            ),
+          ),
     );
   }
 }
